0a7bca4694b125e156a3004411113e5bc1e66579
chechin2000 <96448366+chechin2000@users.noreply.github.com>
2022-06-27 16:47:25 -0600

    Add files via upload

---
+++ scripts/index.js
@@ -0,0 +1,30 @@
+const feature = document.querySelector(".features-menu");
+const featureOptions = document.querySelector(".feature-options");
+const featureimg = document.getElementById("featureImg");
+
+const company = document.querySelector(".company-menu");
+const companyOptions = document.querySelector(".company-options");
+const companyImg = document.getElementById("companyImg");
+
+let featureActive = false;
+let companyActive = false;
+
+const showOptions = (btn, blockOptions, img, active) => {
+  btn.addEventListener("click", () => {
+    const widthWindow = window.innerWidth;
+    const positionValue = widthWindow > 900 ? "absolute" : "relative";
+    if (active === false) {
+      blockOptions.style.display = "block";
+      blockOptions.style.position = positionValue;
+      img.setAttribute("src", "../images/icon-arrow-up.svg");
+      active = true;
+    } else {
+      blockOptions.style.display = "none";
+      img.setAttribute("src", "../images/icon-arrow-down.svg");
+      active = false;
+    }
+  });
+};
+
+showOptions(feature, featureOptions, featureimg, featureActive);
+showOptions(company, companyOptions, companyImg, companyActive);
